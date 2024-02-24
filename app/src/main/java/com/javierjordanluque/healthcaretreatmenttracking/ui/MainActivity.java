886fa198ad9d8d0100379723223529715c996c6d
Javier Jordán Luque <javierjordanluque@gmail.com>
2024-02-24 15:17:38 +0100

    Created ui folder and moved MainActivity.java to it

--- app/src/main/java/com/javierjordanluque/healthcaretreatmenttracking/ui/MainActivity.java
+++ app/src/main/java/com/javierjordanluque/healthcaretreatmenttracking/ui/MainActivity.java
@@ -1,8 +1,12 @@
-package com.javierjordanluque.healthcaretreatmenttracking;
+package com.javierjordanluque.healthcaretreatmenttracking.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.javierjordanluque.healthcaretreatmenttracking.R;
+import com.javierjordanluque.healthcaretreatmenttracking.db.DatabaseHelper;
 
 public class MainActivity extends AppCompatActivity {
 
