0f8378f4e5cae557c1d0f59ebd07f2d559a14c9d
Javier Jordán Luque <javierjordanluque@gmail.com>
2024-03-01 17:43:54 +0100

    Added Treatment.java and TreatmentRepository.java to models and repositories folders respectively
    - Treatment.java methods to-do: filterAppointments(boolean
      pastAppointments, boolean pendingAppointments)

---
+++ app/src/main/java/com/javierjordanluque/healthcaretreatmenttracking/db/repositories/TreatmentRepository.java
@@ -0,0 +1,167 @@
+package com.javierjordanluque.healthcaretreatmenttracking.db.repositories;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.javierjordanluque.healthcaretreatmenttracking.db.BaseRepository;
+import com.javierjordanluque.healthcaretreatmenttracking.models.Treatment;
+import com.javierjordanluque.healthcaretreatmenttracking.models.User;
+import com.javierjordanluque.healthcaretreatmenttracking.models.enumerations.TreatmentCategory;
+import com.javierjordanluque.healthcaretreatmenttracking.util.SerializationUtils;
+import com.javierjordanluque.healthcaretreatmenttracking.util.security.CipherData;
+import com.javierjordanluque.healthcaretreatmenttracking.util.security.SecurityService;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TreatmentRepository extends BaseRepository<Treatment> {
+    private static final String TABLE_NAME = "TREATMENT";
+    private Context context;
+    private final String ID = "id";
+    private final String USER_ID = "user_id";
+    private final String TITLE = "title";
+    private final String TITLE_IV = "title_iv";
+    private final String START_DATE = "start_date";
+    private final String START_DATE_IV = "start_date_iv";
+    private final String END_DATE = "end_date";
+    private final String END_DATE_IV = "end_date_iv";
+    private final String DIAGNOSIS = "diagnosis";
+    private final String DIAGNOSIS_IV = "diagnosis_iv";
+    private final String CATEGORY = "category";
+    private final String CATEGORY_IV = "category_iv";
+
+    public TreatmentRepository(Context context) {
+        super(TABLE_NAME, context);
+        this.context = context;
+    }
+
+    @Override
+    protected ContentValues getContentValues(Treatment treatment) {
+        ContentValues contentValues = new ContentValues();
+
+        if (treatment.getUser() != null)
+            contentValues.put(USER_ID, treatment.getUser().getId());
+        if (treatment.getTitle() != null) {
+            try {
+                CipherData cipherData = SecurityService.encrypt(SerializationUtils.serialize(treatment.getTitle()));
+                contentValues.put(TITLE, cipherData.getEncryptedData());
+                contentValues.put(TITLE_IV, cipherData.getInitializationVector());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        if (treatment.getStartDate() != null) {
+            try {
+                CipherData cipherData = SecurityService.encrypt(SerializationUtils.serialize(treatment.getStartDate()));
+                contentValues.put(START_DATE, cipherData.getEncryptedData());
+                contentValues.put(START_DATE_IV, cipherData.getInitializationVector());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        if (treatment.getEndDate() != null) {
+            try {
+                CipherData cipherData = SecurityService.encrypt(SerializationUtils.serialize(treatment.getEndDate()));
+                contentValues.put(END_DATE, cipherData.getEncryptedData());
+                contentValues.put(END_DATE_IV, cipherData.getInitializationVector());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        if (treatment.getDiagnosis() != null) {
+            try {
+                CipherData cipherData = SecurityService.encrypt(SerializationUtils.serialize(treatment.getDiagnosis()));
+                contentValues.put(DIAGNOSIS, cipherData.getEncryptedData());
+                contentValues.put(DIAGNOSIS_IV, cipherData.getInitializationVector());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        if (treatment.getCategory() != null) {
+            try {
+                CipherData cipherData = SecurityService.encrypt(SerializationUtils.serialize(treatment.getCategory()));
+                contentValues.put(CATEGORY, cipherData.getEncryptedData());
+                contentValues.put(CATEGORY_IV, cipherData.getInitializationVector());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return contentValues;
+    }
+
+    @Override
+    @SuppressLint("Range")
+    protected Treatment cursorToItem(Cursor cursor) {
+        UserRepository userRepository = new UserRepository(context);
+        User user = userRepository.findById(cursor.getLong(cursor.getColumnIndex(USER_ID)));
+
+        CipherData cipherData = new CipherData(cursor.getBlob(cursor.getColumnIndex(TITLE)), cursor.getBlob(cursor.getColumnIndex(TITLE_IV)));
+        String title = null;
+        try {
+            title = (String) SerializationUtils.deserialize(SecurityService.decrypt(cipherData), String.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        cipherData = new CipherData(cursor.getBlob(cursor.getColumnIndex(START_DATE)), cursor.getBlob(cursor.getColumnIndex(START_DATE_IV)));
+        LocalDate startDate = null;
+        try {
+            startDate = (LocalDate) SerializationUtils.deserialize(SecurityService.decrypt(cipherData), LocalDate.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        cipherData = new CipherData(cursor.getBlob(cursor.getColumnIndex(END_DATE)), cursor.getBlob(cursor.getColumnIndex(END_DATE_IV)));
+        LocalDate endDate = null;
+        try {
+            endDate = (LocalDate) SerializationUtils.deserialize(SecurityService.decrypt(cipherData), LocalDate.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        cipherData = new CipherData(cursor.getBlob(cursor.getColumnIndex(DIAGNOSIS)), cursor.getBlob(cursor.getColumnIndex(DIAGNOSIS_IV)));
+        String diagnosis = null;
+        try {
+            diagnosis = (String) SerializationUtils.deserialize(SecurityService.decrypt(cipherData), String.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        cipherData = new CipherData(cursor.getBlob(cursor.getColumnIndex(DIAGNOSIS)), cursor.getBlob(cursor.getColumnIndex(DIAGNOSIS_IV)));
+        TreatmentCategory category = null;
+        try {
+            category = (TreatmentCategory) SerializationUtils.deserialize(SecurityService.decrypt(cipherData), TreatmentCategory.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Treatment treatment = new Treatment(null, user, title, startDate, endDate, diagnosis, category);
+        treatment.setId(cursor.getLong(cursor.getColumnIndex(ID)));
+
+        return treatment;
+    }
+
+    public List<Treatment> findUserTreatments(long userId) {
+        List<Treatment> treatments = new ArrayList<>();
+        SQLiteDatabase db = open();
+
+        String selection = USER_ID + "=?";
+        String[] selectionArgs = {String.valueOf(userId)};
+        Cursor cursor = db.query(TABLE_NAME, null, selection, selectionArgs, null, null, null);
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                Treatment treatment = cursorToItem(cursor);
+                treatments.add(treatment);
+            }
+            cursor.close();
+        }
+
+        close(db);
+        return treatments;
+    }
+}
---
+++ app/src/main/java/com/javierjordanluque/healthcaretreatmenttracking/models/Treatment.java
@@ -0,0 +1,249 @@
+package com.javierjordanluque.healthcaretreatmenttracking.models;
+
+import android.content.Context;
+
+import com.javierjordanluque.healthcaretreatmenttracking.db.repositories.MedicineRepository;
+import com.javierjordanluque.healthcaretreatmenttracking.db.repositories.TreatmentRepository;
+import com.javierjordanluque.healthcaretreatmenttracking.models.enumerations.TreatmentCategory;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Treatment implements Identifiable {
+    private long id;
+    private User user;
+    private String title;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String diagnosis;
+    private TreatmentCategory category;
+    private List<Medicine> medicines;
+    private List<Step> steps;
+    private List<Symptom> symptoms;
+    private List<Question> questions;
+    private List<MedicalAppointment> appointments;
+
+    public Treatment(Context context, User user, String title, LocalDate startDate, LocalDate endDate, String diagnosis, TreatmentCategory category) {
+        this.user = user;
+        this.title = title;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.diagnosis = diagnosis;
+        this.category = category;
+
+        if (user != null)
+            user.addTreatment(context, this);
+    }
+
+    public void modifyTreatment(Context context, String title, LocalDate startDate, LocalDate endDate, String diagnosis, TreatmentCategory category) {
+        if (!this.title.equals(title))
+            setTitle(title);
+        if (!this.startDate.equals(startDate))
+            setStartDate(startDate);
+        if (!this.endDate.equals(endDate))
+            setEndDate(endDate);
+        if (!this.diagnosis.equals(diagnosis))
+            setDiagnosis(diagnosis);
+        if (!this.category.equals(category))
+            setCategory(category);
+
+        Treatment treatment = new Treatment(null, null, this.title, this.startDate, this.endDate, this.diagnosis, this.category);
+        treatment.setId(this.id);
+
+        TreatmentRepository treatmentRepository = new TreatmentRepository(context);
+        treatmentRepository.update(treatment);
+    }
+
+    protected void addMedicine(Context context, Medicine medicine) {
+        if (context != null) {
+            MedicineRepository medicineRepository = new MedicineRepository(context);
+            medicine.setId(medicineRepository.insert(medicine));
+        }
+        medicines.add(medicine);
+    }
+
+    public void removeMedicine(Context context, Medicine medicine) {
+        MedicineRepository medicineRepository = new MedicineRepository(context);
+        medicineRepository.delete(medicine);
+        medicines.remove(medicine);
+    }
+
+    protected void addStep(Context context, Step step) {
+        if (context != null) {
+            StepRepository stepRepository = new StepRepository(context);
+            step.setId(stepRepository.insert(step));
+        }
+        steps.add(step);
+    }
+
+    public void removeStep(Context context, Step step) {
+        StepRepository stepRepository = new StepRepository(context);
+        stepRepository.delete(step);
+        steps.remove(step);
+    }
+
+    protected void addSymptom(Context context, Symptom symptom) {
+        if (context != null) {
+            SymptomRepository symptomRepository = new SymptomRepository(context);
+            symptom.setId(symptomRepository.insert(symptom));
+        }
+        symptoms.add(symptom);
+    }
+
+    public void removeSymptom(Context context, Symptom symptom) {
+        SymptomRepository symptomRepository = new SymptomRepository(context);
+        symptomRepository.delete(symptom);
+        symptoms.remove(symptom);
+    }
+
+    protected void addQuestion(Context context, Question question) {
+        if (context != null) {
+            QuestionRepository questionRepository = new QuestionRepository(context);
+            question.setId(questionRepository.insert(question));
+        }
+        questions.add(question);
+    }
+
+    public void removeQuestion(Context context, Question question) {
+        QuestionRepository questionRepository = new QuestionRepository(context);
+        questionRepository.delete(question);
+        questions.remove(question);
+    }
+
+    protected void addAppointment(Context context, MedicalAppointment appointment) {
+        if (context != null) {
+            MedicalAppointmentRepository medicalAppointmentRepository = new MedicalAppointmentRepository(context);
+            appointment.setId(medicalAppointmentRepository.insert(appointment));
+        }
+        appointments.add(appointment);
+    }
+
+    public void removeAppointment(Context context, MedicalAppointment appointment) {
+        MedicalAppointmentRepository medicalAppointmentRepository = new MedicalAppointmentRepository(context);
+        medicalAppointmentRepository.delete(appointment);
+        appointments.remove(appointment);
+    }
+
+    public List<MedicalAppointment> filterAppointments(boolean pastAppointments, boolean pendingAppointments) {
+        // @TODO
+        return null;
+    }
+
+    @Override
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    private void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    private void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    private void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    private void setDiagnosis(String diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+
+    public TreatmentCategory getCategory() {
+        return category;
+    }
+
+    private void setCategory(TreatmentCategory category) {
+        this.category = category;
+    }
+
+    public List<Medicine> getMedicines() {
+        if (medicines == null) {
+            MedicineRepository medicineRepository = new MedicineRepository(context);
+            setMedicines(medicineRepository.findTreatmentMedicines(this.id));
+        }
+
+        return medicines;
+    }
+
+    private void setMedicines(List<Medicine> medicines) {
+        this.medicines = medicines;
+    }
+
+    public List<Step> getSteps() {
+        if (steps == null) {
+            StepRepository stepRepository = new StepRepository(context);
+            setSteps(stepRepository.findTreatmentSteps(this.id));
+        }
+
+        return steps;
+    }
+
+    private void setSteps(List<Step> steps) {
+        this.steps = steps;
+    }
+
+    public List<Symptom> getSymptoms() {
+        if (symptoms == null) {
+            SymptomRepository symptomRepository = new SymptomRepository(context);
+            setSymptoms(symptomRepository.findTreatmentSymptoms(this.id));
+        }
+
+        return symptoms;
+    }
+
+    private void setSymptoms(List<Symptom> symptoms) {
+        this.symptoms = symptoms;
+    }
+
+    public List<Question> getQuestions() {
+        if (questions == null) {
+            QuestionRepository questionRepository = new QuestionRepository(context);
+            setQuestions(questionRepository.findTreatmentQuestions(this.id));
+        }
+
+        return questions;
+    }
+
+    private void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
+
+    public List<MedicalAppointment> getAppointments() {
+        if (appointments == null) {
+            MedicalAppointmentRepository medicalAppointmentRepository = new MedicalAppointmentRepository(context);
+            setAppointments(medicalAppointmentRepository.findTreatmentAppointments(this.id));
+        }
+
+        return appointments;
+    }
+
+    private void setAppointments(List<MedicalAppointment> appointments) {
+        this.appointments = appointments;
+    }
+}
