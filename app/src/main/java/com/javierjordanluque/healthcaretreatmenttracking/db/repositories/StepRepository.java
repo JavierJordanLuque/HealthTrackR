7878a68a8d3063de9c687648b8a85ad5b33af14d
Javier Jordán Luque <javierjordanluque@gmail.com>
2024-03-02 18:08:23 +0100

    Added Step.java and StepRepository.java to models and repositories folders respectively

---
+++ app/src/main/java/com/javierjordanluque/healthcaretreatmenttracking/db/repositories/StepRepository.java
@@ -0,0 +1,76 @@
+package com.javierjordanluque.healthcaretreatmenttracking.db.repositories;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.javierjordanluque.healthcaretreatmenttracking.db.BaseRepository;
+import com.javierjordanluque.healthcaretreatmenttracking.models.Step;
+import com.javierjordanluque.healthcaretreatmenttracking.models.Treatment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StepRepository extends BaseRepository<Step> {
+    private static final String TABLE_NAME = "STEP";
+    private final String ID = "id";
+    private final String TREATMENT_ID = "treatment_id";
+    private final String TITLE = "title";
+    private final String DESCRIPTION = "description";
+    private final String NUM_ORDER = "num_order";
+    private Context context;
+
+    public StepRepository(Context context) {
+        super(TABLE_NAME, context);
+        this.context = context;
+    }
+
+    @Override
+    protected ContentValues getContentValues(Step step) {
+        ContentValues contentValues = new ContentValues();
+
+        if (step.getTreatment() != null)
+            contentValues.put(TREATMENT_ID, step.getTreatment().getId());
+        if (step.getTitle() != null)
+            contentValues.put(TITLE, step.getTitle());
+        if (step.getDescription() != null)
+            contentValues.put(DESCRIPTION, step.getDescription());
+        if (step.getNumOrder() >= 0)
+            contentValues.put(NUM_ORDER, step.getNumOrder());
+
+        return contentValues;
+    }
+
+    @Override
+    @SuppressLint("Range")
+    protected Step cursorToItem(Cursor cursor) {
+        TreatmentRepository treatmentRepository = new TreatmentRepository(context);
+        Treatment treatment = treatmentRepository.findById(cursor.getLong(cursor.getColumnIndex(TREATMENT_ID)));
+
+        Step step = new Step(null, treatment, cursor.getString(cursor.getColumnIndex(TITLE)), cursor.getString(cursor.getColumnIndex(DESCRIPTION)), cursor.getInt(cursor.getColumnIndex(NUM_ORDER)));
+        step.setId(cursor.getLong(cursor.getColumnIndex(ID)));
+
+        return step;
+    }
+
+    public List<Step> findTreatmentSteps(long treatmentId) {
+        List<Step> steps = new ArrayList<>();
+        SQLiteDatabase db = open();
+
+        String selection = TREATMENT_ID + "=?";
+        String[] selectionArgs = {String.valueOf(treatmentId)};
+        Cursor cursor = db.query(TABLE_NAME, null, selection, selectionArgs, null, null, null);
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                Step step = cursorToItem(cursor);
+                steps.add(step);
+            }
+            cursor.close();
+        }
+
+        close(db);
+        return steps;
+    }
+}
---
+++ app/src/main/java/com/javierjordanluque/healthcaretreatmenttracking/models/Step.java
@@ -0,0 +1,114 @@
+package com.javierjordanluque.healthcaretreatmenttracking.models;
+
+import android.content.Context;
+
+import com.javierjordanluque.healthcaretreatmenttracking.db.repositories.MultimediaRepository;
+import com.javierjordanluque.healthcaretreatmenttracking.db.repositories.StepRepository;
+
+import java.util.List;
+
+public class Step implements Identifiable {
+    private long id;
+    private Treatment treatment;
+    private String title;
+    private String description;
+    private int numOrder;
+    private List<Multimedia> multimedias;
+
+    public Step(Context context, Treatment treatment, String title, String description, int numOrder) {
+        this.treatment = treatment;
+        this.title = title;
+        this.description = description;
+        this.numOrder = numOrder;
+        this.treatment.addStep(context, this);
+    }
+
+    public Step(){
+        this.numOrder = -1;
+    }
+
+    public void modifyStep(Context context, String title, String description, int numOrder) {
+        Step step = new Step();
+        step.setId(this.id);
+
+        if (!this.title.equals(title)) {
+            setTitle(title);
+            step.setTitle(this.title);
+        }
+        if ((this.description == null && description != null ) || (description != null && !this.description.equals(description))) {
+            setDescription(description);
+            step.setDescription(description);
+        }
+        if (this.numOrder != numOrder) {
+            setNumOrder(numOrder);
+            step.setNumOrder(numOrder);
+        }
+
+        StepRepository stepRepository = new StepRepository(context);
+        stepRepository.update(step);
+    }
+
+    protected void addMultimedia(Context context, Multimedia multimedia) {
+        if (context != null) {
+            MultimediaRepository multimediaRepository = new MultimediaRepository(context);
+            multimedia.setId(multimediaRepository.insert(multimedia));
+        }
+        multimedias.add(multimedia);
+    }
+
+    public void removeMultimedia(Context context, Multimedia multimedia) {
+        MultimediaRepository multimediaRepository = new MultimediaRepository(context);
+        multimediaRepository.delete(multimedia);
+        multimedias.remove(multimedia);
+    }
+
+    @Override
+    public long getId() {
+        return id;
+    }
+
+    private void setId(long id) {
+        this.id = id;
+    }
+
+    public Treatment getTreatment() {
+        return treatment;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    private void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    private void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getNumOrder() {
+        return numOrder;
+    }
+
+    private void setNumOrder(int numOrder) {
+        this.numOrder = numOrder;
+    }
+
+    public List<Multimedia> getMultimedias(Context context) {
+        if (multimedias == null) {
+            MultimediaRepository multimediaRepository = new MultimediaRepository(context);
+            setMultimedias(multimediaRepository.findStepMultimedias(this.id));
+        }
+
+        return multimedias;
+    }
+
+    private void setMultimedias(List<Multimedia> multimedias) {
+        this.multimedias = multimedias;
+    }
+}
