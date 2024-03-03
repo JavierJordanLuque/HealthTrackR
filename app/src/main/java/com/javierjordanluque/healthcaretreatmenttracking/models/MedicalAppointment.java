2ad9e5269815b9828105630bd17c663a6a3ffd00
Javier Jordán Luque <javierjordanluque@gmail.com>
2024-03-03 19:26:12 +0100

    Modified MedicalAppointment.java: Modified constructor
    - Modified constructor MedicalAppointment(Context context, Treatment
      treatment, String purpose, LocalDate date, LocalTime time, Location
      location), which now creates a notification one hour before the
      appointment's date
    - MedicalAppointment.java methods to-do: Constructor MedicalAppointment(Context
      context, Treatment treatment, String purpose, LocalDate date,
      LocalTime time, Location location) must call schedule the notification
      to generate an alarm

--- app/src/main/java/com/javierjordanluque/healthcaretreatmenttracking/models/MedicalAppointment.java
+++ app/src/main/java/com/javierjordanluque/healthcaretreatmenttracking/models/MedicalAppointment.java
@@ -3,10 +3,13 @@ package com.javierjordanluque.healthcaretreatmenttracking.models;
 import android.content.Context;
 
 import com.javierjordanluque.healthcaretreatmenttracking.db.repositories.MedicalAppointmentRepository;
+import com.javierjordanluque.healthcaretreatmenttracking.db.repositories.NotificationRepository;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 public class MedicalAppointment implements Identifiable {
     private long id;
@@ -24,6 +27,15 @@ public class MedicalAppointment implements Identifiable {
         this.time = time;
         this.location = location;
         this.treatment.addAppointment(context, this);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime dateTime = LocalDateTime.parse(this.date + " " + this.time, formatter);
+        long timestamp = dateTime.minusHours(1).atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+        notification = new Notification(this, timestamp);
+        NotificationRepository notificationRepository = new NotificationRepository(context);
+        notification.setId(notificationRepository.insert(notification));
+        // @TODO
+        // Call NotificationScheduler's scheduleNotification method
     }
 
     public MedicalAppointment() {
@@ -56,6 +68,15 @@ public class MedicalAppointment implements Identifiable {
 
     public void modifyMedicalAppointmentNotification(Context context, int notificationTimeHours, int notificationTimeMinutes, boolean notificationStatus) {
         // @TODO
+        // If notification time is different from current notification timestamp then delete current notification from NOTIFICATION table and insert the new notification
+        // In that case, call NotificationScheduler's cancelNotification and then scheduleNotification methods
+    }
+
+    public boolean isPending() {
+        LocalDateTime dateTime = LocalDateTime.of(this.date, this.time);
+        LocalDateTime now = LocalDateTime.now();
+
+        return dateTime.isAfter(now);
     }
 
     @Override
@@ -101,12 +122,5 @@ public class MedicalAppointment implements Identifiable {
 
     private void setLocation(Location location) {
         this.location = location;
-    }
-
-    public boolean isPending() {
-        LocalDateTime dateTime = LocalDateTime.of(this.date, this.time);
-        LocalDateTime now = LocalDateTime.now();
-
-        return dateTime.isAfter(now);
     }
 }
