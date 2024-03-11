cd437ca5fa057e38d3851f592266b68f625f10ab
Javier Jordán Luque <javierjordanluque@gmail.com>
2024-03-11 11:58:53 +0100

    Modified BootReceiver.java: Modified onReceive method
    - Modified onReceive(Context context, Intent intent) method, which now
      restarts all notifications alarms when the device reboots

--- app/src/main/java/com/javierjordanluque/healthcaretreatmenttracking/util/notifications/BootReceiver.java
+++ app/src/main/java/com/javierjordanluque/healthcaretreatmenttracking/util/notifications/BootReceiver.java
@@ -4,11 +4,34 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.javierjordanluque.healthcaretreatmenttracking.db.repositories.NotificationRepository;
+import com.javierjordanluque.healthcaretreatmenttracking.util.exceptions.DBFindException;
+import com.javierjordanluque.healthcaretreatmenttracking.util.exceptions.NotificationException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
-            // @TODO Set all alarms again
+        if (intent.getAction() != null && intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+            NotificationRepository notificationRepository = new NotificationRepository(context);
+            List<Notification> notifications = new ArrayList<>();
+            try {
+                notifications = notificationRepository.findAll();
+            } catch (DBFindException ignored) {
+            }
+
+            for (Notification notification : notifications) {
+                if (notification instanceof MedicationNotification) {
+                    try {
+                        NotificationScheduler.scheduleInexactRepeatingNotification(context, notification);
+                    } catch (NotificationException ignored) {
+                    }
+                } else if (notification instanceof MedicalAppointmentNotification) {
+                    NotificationScheduler.scheduleInexactNotification(context, notification);
+                }
+            }
         }
     }
 }
