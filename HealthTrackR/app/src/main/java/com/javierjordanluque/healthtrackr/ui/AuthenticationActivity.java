c4a65abc38ff4325e03b384f15e25ed31ba500a9
Javier Jordán Luque <javierjordanluque@gmail.com>
2024-03-24 17:46:27 +0100

    Added several styles.xml and strings.xml, and added toolbar.xml and two menu.xml

--- HealthTrackR/app/src/main/java/com/javierjordanluque/healthtrackr/ui/AuthenticationActivity.java
+++ HealthTrackR/app/src/main/java/com/javierjordanluque/healthtrackr/ui/AuthenticationActivity.java
@@ -2,7 +2,10 @@ package com.javierjordanluque.healthtrackr.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.javierjordanluque.healthtrackr.R;
 
@@ -12,5 +15,16 @@ public class AuthenticationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
+
+        Button button = findViewById(R.id.buttonSignUp);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AuthenticationActivity.this, SignUpActivity.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
--- HealthTrackR/app/src/main/java/com/javierjordanluque/healthtrackr/ui/SignUpActivity.java
+++ HealthTrackR/app/src/main/java/com/javierjordanluque/healthtrackr/ui/SignUpActivity.java
@@ -3,7 +3,10 @@ package com.javierjordanluque.healthtrackr.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.javierjordanluque.healthtrackr.R;
 
 public class SignUpActivity extends AppCompatActivity {
@@ -12,5 +15,18 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        AppBarLayout appBarLayout = findViewById(R.id.appBarLayout);
+        MaterialToolbar toolbar = (MaterialToolbar) appBarLayout.getChildAt(0);
+
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle(getString(R.string.authentication_sign_up));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_basic_menu, menu);
+        return true;
     }
 }
