76c767504ba709586f4940372f9a4109c452b447
Javier Jordán Luque <javierjordanluque@gmail.com>
2024-03-27 19:40:47 +0100

    Added NavigationUtils.java to utils folder and modified AndroidManifest.xml to grant permission to Internet

---
+++ HealthTrackR/app/src/main/java/com/javierjordanluque/healthtrackr/util/NavigationUtils.java
@@ -0,0 +1,39 @@
+package com.javierjordanluque.healthtrackr.util;
+
+import android.content.Context;
+import android.content.Intent;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.widget.Toast;
+
+import com.javierjordanluque.healthtrackr.R;
+
+public class NavigationUtils {
+    private static final String USER_MANUAL_URL = "https://github.com/JavierJordanLuque/HealthTrackR/tree/main";
+    public static void openUserManual(Context context) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(USER_MANUAL_URL));
+        context.startActivity(intent);
+    }
+
+    public static void openGoogleMaps(Context context, double latitude, double longitude) {
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        boolean isLocationEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        if (!isLocationEnabled) {
+            Toast.makeText(context, context.getString(R.string.error_location_disabled), Toast.LENGTH_LONG).show();
+        } else {
+            Uri gmmIntentUri = Uri.parse("google.navigation:q=" + latitude + "," + longitude);
+
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+            mapIntent.setPackage("com.google.android.apps.maps");
+
+            if (mapIntent.resolveActivity(context.getPackageManager()) != null) {
+                context.startActivity(mapIntent);
+            } else {
+                Uri webIntentUri = Uri.parse("https://www.google.com/maps/dir/?api=1&destination=" + latitude + "," + longitude);
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webIntentUri);
+                context.startActivity(webIntent);
+            }
+        }
+    }
+}
